d5802d11880c23a8d21a4e1e5cd5d03e24187a44
KrulH <mehmetcelik4@gmail.com>
2016-10-01 09:48:40 +0300

    urun page photos refactor

--- src/js/pages/Urunler.js
+++ src/js/pages/Urunler.js
@@ -5,36 +5,37 @@ import Photo from "../components/Photo";
 export default class Urunler extends React.Component {
   render() {
 
-    var urun ={name: "Fag rulman",
+    var urun = [{name: "Fag rulman",
       photo:["../js/images/fag-rulman.jpg",
         "../js/images/lineer-rulman.jpg"],
-      text: "Sanayi, İğneli, Lineer ve Otomotiv rulman serileri"};
-    var urun1 ={name: "Hitachi Zincir",
+      text: "Sanayi, İğneli, Lineer ve Otomotiv rulman serileri"}
+    ,{name: "Hitachi Zincir",
       photo:["../js/images/hitachi.JPG","../js/images/hitachi2.JPG"],
-      text: "Asa, Bs , Ziraai zincir grupları" };
-    var urun2 ={name: "Tsubaki Zincir",
+      text: "Asa, Bs , Ziraai zincir grupları" }
+    ,{name: "Tsubaki Zincir",
       photo:["../js/images/tsubaki.jpg","../js/images/ZİNCİR.jpg"],
-      text:"Asa, Bs , Ziraai zincir grupları"};
-    var urun3 ={name: "Promak Mekanik Salmastra",
+      text:"Asa, Bs , Ziraai zincir grupları"}
+    ,{name: "Promak Mekanik Salmastra",
       photo:["../js/images/salmastra.jpg"],
-      text:null};
-    var urun4 ={name: " Kama ve Çiftel Teker",
+      text:null}
+    ,{name: " Çiftel ve Kama Teker",
       photo:["../js/images/teker.jpg"],
-      text:null};
-    var urun5 ={name: "Hassel Dişli",
+      text:null}
+    ,{name: "Hassel Dişli",
       photo:["../js/images/disli.jpg"],
-      text:null};
+      text:null}];
 
-    var list = [
-      <Photo urun={urun} height="300" width="300"/>,
-      <Photo urun={urun1} height="300" width="300"/>,
-      <Photo urun={urun2} height="300" width="300"/>,
-      <Photo urun={urun3} height="300" width="300"/>,
-      <Photo urun={urun4} height="300" width="300"/>,
-      <Photo urun={urun5} height="300" width="300"/>];
+
+
+      const PhotoComponents = urun.map((photos) => {
+        return <Photo key={photos.id} {...photos} height="300" width="300"/>;
+      });
+
+
+
     return (
         <div>
-          {list}
+          {PhotoComponents}
         </div>
     );
   }
